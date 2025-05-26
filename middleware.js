19844255b1a27a784d504c5c724b9b8ac6d8154d
Mohd kaif <Mohd kaif>
2025-05-26 13:19:59 +0530

    some updation in middlewere

--- middleware.js
+++ middleware.js
@@ -10,7 +10,7 @@ module.exports.isLoggedIn = (req,res,next)=>{
         req.flash("error","You must be logged in to create listing!");
         return res.redirect("/login");
     }
-    next();
+    next()
 };
 module.exports.saveRedirectUrl = (req,res,next)=>{
     if(req.session.redirectUrl){
